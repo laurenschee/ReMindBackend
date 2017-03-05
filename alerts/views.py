02cf5ee0c492a05d558897218de7d815fa6fe09d
Lauren Chee <lauren@magniware.ca>
2017-03-05 14:44:32 -0500

    Updated message that is sent to phone

--- alerts/views.py
+++ alerts/views.py
@@ -13,8 +13,10 @@ def send_alert(request):
         send_twilio_message('4163718808', 'lolcats make me hungry')
     elif request.method == 'POST':
         print request.body
+        parameter_list = [p.split('=') for p in request.body.split('&')]
+        parameters = dict(parameter_list)
         try:
-            sensor = Sensor.objects.get(arduino_id=123)
+            sensor = Sensor.objects.get(arduino_id=parameters['UUID'])
             print "SENSOR"
             user = sensor.owner
             print "got USEr"
@@ -28,10 +30,25 @@ def send_alert(request):
             print "got FIRST NAME"
             home_name = sensor.home.nickname
             print "got HOME NAME"
-            message = 'Hi '+first_name+' this is an alert from '+sensor_name+' in '+home_name
+            message = make_message(first_name, sensor_name, home_name, parameters)
             send_twilio_message(phone, message)
             print "SENT MESSAGE"
         except:
             pass
 
     return HttpResponse("sent message")
+
+
+def make_message(first_name, sensor_name, home_name, parameters):
+    if parameters['above']:
+        above_below = 'above'
+    else:
+        above_below = 'below'
+    message = 'Hi '+first_name+\
+              ', this is an alert from '+sensor_name+\
+              ' in '+home_name+\
+              '. It has measured a(n) '+parameters['measurement']+\
+              ' at '+parameters['value']+\
+              ' centimeters for '+parameters['duration']+\
+              ' minutes, which is '+above_below+' where it should be.'
+    return message
